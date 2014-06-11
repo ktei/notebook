7f71b4c0843d5693f6ecfec2a12cf5779b854e8a
Rui Cheng <ktei2008@gmail.com>
2014-06-11 16:37:42 +1000

    added mongodb dependency

--- app.js
+++ app.js
@@ -8,11 +8,12 @@
  , user = require('./routes/user')
  , http = require('http')
  , path = require('path')
- , lessMiddleware = require('less-middleware');
+ , lessMiddleware = require('less-middleware')
+ , mongo = require('mongodb');
 
  var app = express();
 
- app.configure(function(){
+ app.configure(function() {
  	app.set('port', process.env.PORT || 3000);
  	app.set('views', __dirname + '/views');
  	app.set('view engine', 'jade');
@@ -20,7 +21,7 @@
  	app.use(express.logger('dev'));
  	app.use(express.bodyParser());
  	app.use(express.methodOverride());
- 	app.use(express.cookieParser('your secret here'));
+ 	app.use(express.cookieParser('km5jpVEi'));
  	app.use(express.session());
  	app.use(app.router);
  	app.use(lessMiddleware(path.join(__dirname, 'public')));
@@ -28,13 +29,13 @@
  	app.use(express.static(path.join(__dirname, 'bower_components')));
  });
 
- app.configure('development', function(){
+ app.configure('development', function() {
  	app.use(express.errorHandler());
  });
 
  app.get('/', routes.index);
  app.get('/users', user.list);
 
- http.createServer(app).listen(app.get('port'), function(){
+ http.createServer(app).listen(app.get('port'), function() {
  	console.log("Express server listening on port " + app.get('port'));
  });
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+ exports.index = function(req, res) {
+ 	var db = require('mongodb').MongoClient;
+ 	db.connect("mongodb://localhost:27017/WorkMemo", function(err, db) {
+ 		if (!err) {
+ 			res.render('index', { title: 'Success!' });
+ 		} else {
+ 			res.render('index', { title: 'Failure!' });
+ 		}
+ 	});
+ 	
+ };
