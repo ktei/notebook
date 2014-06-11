59473f75acdb989e148839fa27a52b62074226d0
Rui Cheng <ktei2008@gmail.com>
2014-06-11 17:46:42 +1000

    connect to remote mongolab trial

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@
 
  exports.index = function(req, res) {
  	var db = require('mongodb').MongoClient;
- 	db.connect("mongodb://localhost:27017/WorkMemo", function(err, db) {
+ 	db.connect("mongodb://ktei@ds041248.mongolab.com:41248/notebook", function(err, db) {
  		if (!err) {
  			res.render('index', { title: 'Success!' });
  		} else {
- 			res.render('index', { title: 'Failure!' });
+ 			res.render('index', { title: 'Failure!', error: err });
  		}
  	});
  	
